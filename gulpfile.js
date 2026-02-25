896ebc6567c81130e79fd6e235deee53a42030cc
Ana <analuizagomeslima@hotmail.com>
2026-02-25 20:17:09 -0300

    aula 5 modulo 16

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,16 @@
 const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const sourcemaps = require('gulp-sourcemaps');
+
+function compilaSass() {
+    return gulp.src('./Front-End/modulo16/aula/source/styles/main.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass({
+            style: 'compressed' // não é mais outputStyle
+        }))
+        .pipe(sourcemaps.write('./maps'))
+        .pipe(gulp.dest('./Front-End/modulo16/aula/build/styles'));
+}
 
 function funcaoPadrao(callback) {
     setTimeout(function(){
@@ -22,3 +34,7 @@ function dizTchau() {
 
 exports.default = gulp.parallel(funcaoPadrao, dizOi);
 exports.dizOi = dizOi;
+exports.sass = compilaSass;
+exports.watch = function() {
+    gulp.watch('./Front-End/modulo16/aula/source/styles/*.scss', {ignoreInitial: false}, gulp.series(compilaSass));
+}
