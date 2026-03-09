d8aa01aff3e13afcf7b721d5c80a2754ded9cb8c
Ana <analuizagomeslima@hotmail.com>
2026-03-09 16:48:17 -0300

    adicionando exercicio_gulp modulo16

--- Front-End/modulo16/exercicio_gulp/gulpfile.js
+++ Front-End/modulo16/exercicio_gulp/gulpfile.js
@@ -4,13 +4,27 @@ const imagemin = require('gulp-imagemin');
 const uglify = require('gulp-uglify');
 
 function comprimeImagens(){
-
+    return gulp.src('./source/images/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('./build/images'))
 }
 
 function comprimeJavaScript(){
-
+    return gulp.src('./source/scripts/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('./build/scripts'))
 }
 
 function compilaSass(){
-    
+    return gulp.src('./source/styles/main.scss')
+    .pipe(sass({
+        style: 'compressed'
+    }))
+    .pipe(gulp.dest('./build/styles'))
+}   
+
+exports.default = function() {
+    gulp.watch('./source/styles/*.scss', {ignoreInitial: false}, gulp.series(compilaSass));
+    gulp.watch('./source/scripts/*.js', {ignoreInitial: false}, gulp.series(comprimeJavaScript));
+    gulp.watch('./source/images/*', {ignoreInitial: false}, gulp.series(comprimeImagens));
 }
