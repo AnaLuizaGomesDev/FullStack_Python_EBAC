71dcb05959e9af339adbdeb7429b9141ab46a602
Ana <analuizagomeslima@hotmail.com>
2026-04-20 21:02:21 -0300

    refactor(tabs): rename tabs script to showsTabs

--- Front-End/modulo21/disney-plus-landing-page/src/scripts/main.js
+++ Front-End/modulo21/disney-plus-landing-page/src/scripts/main.js
@@ -1,6 +1,8 @@
-import { initTabs } from './components/tabs.js';
+import { initTabs } from './components/showsTabs.js';
+import { initFAQ } from './components/faqQuestions.js';
 
 document.addEventListener('DOMContentLoaded', function(){
     initTabs();
+    initFAQ();
 })
 
